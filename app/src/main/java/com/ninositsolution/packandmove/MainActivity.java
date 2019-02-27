c8c4ead6a15b4e24585d047e9ed7aeebaf80a733
ninos <arunprasadh.s@gmail.com>
2019-02-27 19:48:37 +0530

    Initial commit by Arunprasadh on 27/02/2019 05:30 PM
                                      Navigation options added for MainActivity screen and slider pages hsa been reworked

--- app/src/main/java/com/ninositsolution/packandmove/MainActivity.java
+++ app/src/main/java/com/ninositsolution/packandmove/MainActivity.java
@@ -2,14 +2,20 @@ package com.ninositsolution.packandmove;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.ninositsolution.packandmove.International.InternationalActivity;
@@ -21,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout officeremovallayout;
     ImageView logOut;
     Button logOutYesAlertBtn,logOutNoAlertBtn;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
 
     LinearLayout factory,packaging,carton,longDistance,truck,doortodoor,temproary,international,labour,custom;
 
@@ -29,6 +38,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+
+                switch(id)
+                {
+                    case R.id.account:
+                        Toast.makeText(MainActivity.this, "My Account",Toast.LENGTH_SHORT).show();
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                    case R.id.mycart:
+                        Toast.makeText(MainActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
+
+
+
 
         residentialLayout = (LinearLayout)findViewById(R.id.residential_removal_layout);
         officeremovallayout = (LinearLayout)findViewById(R.id.office_removal_layout);
@@ -168,6 +209,16 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(t.onOptionsItemSelected(item))
+            return true;
 
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
+
+
