22f3730cca6f742c9787b2a82d2b1fef9dbbb874
ninos <arunprasadh.s@gmail.com>
2019-04-11 18:28:03 +0530

    11/04/2019 06:30 PM
    Need to complete switching of air and sea layouts based on radio button selection in custom clearance.

--- app/src/main/java/com/ninositsolution/packandmove/longdistancetransportation/LongDistanceActivity.java
+++ app/src/main/java/com/ninositsolution/packandmove/longdistancetransportation/LongDistanceActivity.java
@@ -2,14 +2,56 @@ package com.ninositsolution.packandmove.longdistancetransportation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.ninositsolution.packandmove.R;
 
 public class LongDistanceActivity extends AppCompatActivity {
 
+    LinearLayout packagingYesLayout;
+    RadioGroup packagingRadioGroup;
+    RadioButton packagingYesBtn,packagingNoBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_long_distance);
+        packagingYesLayout = findViewById(R.id.packagingYesLayout);
+        packagingYesLayout.setVisibility(View.GONE);
+        packagingRadioGroup = findViewById(R.id.packagingRadioGrp);
+        packagingYesBtn = findViewById(R.id.packagingYesRadioBtn);
+        packagingNoBtn = findViewById(R.id.packagingNoRadioBtn);
+
+        packagingRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.packagingYesRadioBtn)
+                {
+                    if (packagingYesLayout.getVisibility()==View.GONE)
+                    {
+                        packagingYesLayout.setVisibility(View.VISIBLE);
+                    }
+                    else
+                    {
+                        packagingYesLayout.setVisibility(View.GONE);
+                    }
+                }
+                if (checkedId == R.id.packagingNoRadioBtn)
+                {
+                    if (packagingYesLayout.getVisibility() == View.VISIBLE)
+                    {
+                        packagingYesLayout.setVisibility(View.GONE);
+                    }
+
+                }
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/ninositsolution/packandmove/truckrental/TruckRentalActivity.java
+++ app/src/main/java/com/ninositsolution/packandmove/truckrental/TruckRentalActivity.java
@@ -2,14 +2,58 @@ package com.ninositsolution.packandmove.truckrental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.ninositsolution.packandmove.R;
 
 public class TruckRentalActivity extends AppCompatActivity {
 
+    LinearLayout yesLabourLayout;
+    RadioGroup labourSupplyRadioGrp;
+    RadioButton labourYesRadioBtn;
+    RadioButton labourNoRadioBtn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_truck_rental);
+        yesLabourLayout = findViewById(R.id.yesLabourLayout);
+        yesLabourLayout.setVisibility(View.GONE);
+        labourSupplyRadioGrp = findViewById(R.id.labourSupplyRadioGrp);
+        labourYesRadioBtn = findViewById(R.id.labourYesRadioBtn);
+        labourNoRadioBtn = findViewById(R.id.labourNoRadioBtn);
+
+        labourSupplyRadioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.labourYesRadioBtn)
+                {
+                    if (yesLabourLayout.getVisibility() == View.GONE)
+                    {
+                        yesLabourLayout.setVisibility(View.VISIBLE);
+                    }
+                    else
+                    {
+                        yesLabourLayout.setVisibility(View.GONE);
+                    }
+                }
+
+                if (checkedId == R.id.labourNoRadioBtn)
+                {
+                    if (yesLabourLayout.getVisibility() == View.VISIBLE)
+                    {
+                        yesLabourLayout.setVisibility(View.GONE);
+                    }
+                }
+
+            }
+        });
+
+
     }
 }
